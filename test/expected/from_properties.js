5606ff37bdbb2483f8eee099a17bf4671daa3839
pcruz <pedro.cruz@log.pt>
2014-09-24 11:59:16 +0100

    Add tests and fixtures

---
+++ test/expected/from_properties.js
@@ -0,0 +1,32 @@
+define({
+    "root": {
+        "base": {
+            "another": {
+                "to": {
+                    "string": "String",
+                    "number": "2",
+                    "float": "2.1"
+                }
+            },
+            "path": {
+                "to": {
+                    "string": "String",
+                    "number": "1",
+                    "float": "1.2",
+                    "another": {
+                        "string": "inner",
+                        "inner": {
+                            "again": "again",
+                            "and": {
+                                "again": "again",
+                                "big": "big string to see if \"+\" ...everything...is....in....place"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "pt-pt": true,
+    "es-es": true
+});
